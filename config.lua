7d835b23721733276156ba706af6d7a0dc111d0d
ho2xsein <59254677+ho2xsein@users.noreply.github.com>
2021-05-06 16:42:35 +0430

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,14 +1,14 @@
 return {
-  api_id = ,
-  api_hash = '',
-  session_name = '',
-  token = '',
-  channel ='',
-  o_username = '',
-  gplink = '',
-  robot_username = '',
-  owner_ids = {
-	},
+  api_id =4649062 ,
+  api_hash = '4159445745ceabf6274702659b3fdf1e',
+  session_name = 'hi',
+  token = '1741247185:AAE8eg-u0JypmdbPQdMGJ_stYGq6vQ18Fxo',
+  channel ='@sahand_online',
+  o_username = '@sahandd776',
+  gplink = '@sahand_online',
+  robot_username = '@sahand_helper_bot',
+  owner_ids = {1722237594
+  },
   plugins = {
   }, -- Leave blank
   emojis = {
